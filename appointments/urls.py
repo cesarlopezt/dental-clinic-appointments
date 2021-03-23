23eb46f36c04b21f66e64c7d9c49865788d702c0
calt08 <cesarlopez2808@gmail.com>
2021-03-23 09:08:04 -0400

    Add Create Patients page, remove Person model

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import ProfileView, PatientListView
+from .views import PatientCreateView, ProfileView, PatientListView
 
 urlpatterns = [
     path('', ProfileView.as_view(), name="profile"),
     path('patients/', PatientListView.as_view(), name="patient-list"),
+    path('patients/create/', PatientCreateView.as_view(), name="patient-create"),
 
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -1,8 +1,11 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic.detail import DetailView
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+
+from django.urls import reverse_lazy
 
 from django.contrib.auth.models import User
 from .models import Patient
@@ -23,6 +26,13 @@ class PatientListView(ListView):
     template_name = 'patients/list.html'
     context_object_name = 'patients'
 
+
+@method_decorator(login_required, name='dispatch')
+class PatientCreateView(CreateView):
+    model = Patient
+    fields = "__all__"
+    template_name = 'patients/create.html'
+    success_url = reverse_lazy('patient-list')
 
 # def get_context_data(self, **kwargs):
 #     context = super(ProfileView, self).get_context_data(**kwargs)
