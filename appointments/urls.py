4a4511a72f7d98bf02580bee1779bf97d536f1e7
calt08 <cesarlopez2808@gmail.com>
2021-03-26 02:22:01 -0400

    Add views for appointments and refactor on other views (Close a tag, navbar items are always marked)

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from .views import (ProfileView, PatientCreateView, PatientListView,
-                    PatientUpdateView, PatientDetailView, PatientDeleteView,
-                    DentistListView
-                    )
+from .views import (
+    ProfileView, PatientCreateView, PatientListView,
+    PatientUpdateView, PatientDetailView, PatientDeleteView,
+    DentistListView, AppointmentListView, AppointmentDetailView,
+    AppointmentCreateView, AppointmentDeleteView, AppointmentUpdateView
+)
 
 urlpatterns = [
     path('', ProfileView.as_view(), name="profile"),
     path('dentists/', DentistListView.as_view(), name="dentist-list"),
+
     path('patients/', PatientListView.as_view(), name="patient-list"),
     path('patients/create/', PatientCreateView.as_view(), name="patient-create"),
     path('patients/<int:pk>/',
@@ -16,4 +19,13 @@ urlpatterns = [
     path('patients/<int:pk>/delete/',
          PatientDeleteView.as_view(), name="patient-delete"),
 
+    path('appointments/', AppointmentListView.as_view(), name="appointment-list"),
+    path('appointments/create/', AppointmentCreateView.as_view(),
+         name="appointment-create"),
+    path('appointments/<int:pk>/',
+         AppointmentDetailView.as_view(), name="appointment-detail"),
+    path('appointments/<int:pk>/update/',
+         AppointmentUpdateView.as_view(), name="appointment-update"),
+    path('appointments/<int:pk>/delete/',
+         AppointmentDeleteView.as_view(), name="appointment-delete"),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -5,10 +5,10 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from django.contrib.auth.models import User
-from .models import Patient, Dentist
+from .models import Patient, Dentist, Appointment
 
 
 @method_decorator(login_required, name='dispatch')
@@ -58,11 +58,21 @@ class PatientCreateView(CreateView):
     template_name = 'patients/create.html'
     success_url = reverse_lazy('patient-list')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patients_navbar'] = "selected"
+        return context
+
 
 @method_decorator(login_required, name='dispatch')
 class PatientDetailView(DetailView):
     model = Patient
     template_name = 'patients/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patients_navbar'] = "selected"
+        return context
 
 
 @method_decorator(login_required, name='dispatch')
@@ -70,7 +80,14 @@ class PatientUpdateView(UpdateView):
     model = Patient
     fields = "__all__"
     template_name = 'patients/update.html'
-    success_url = reverse_lazy('patient-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patients_navbar'] = "selected"
+        return context
+
+    def get_success_url(self):
+        return reverse('patient-detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(login_required, name='dispatch')
@@ -78,3 +95,72 @@ class PatientDeleteView(DeleteView):
     model = Patient
     template_name = 'patients/delete.html'
     success_url = reverse_lazy('patient-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patients_navbar'] = "selected"
+        return context
+
+
+# Appointment
+@method_decorator(login_required, name='dispatch')
+class AppointmentListView(ListView):
+    model = Appointment
+    template_name = 'appointments/list.html'
+    context_object_name = 'appointments'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments_navbar'] = "selected"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class AppointmentCreateView(CreateView):
+    model = Appointment
+    fields = "__all__"
+    template_name = 'appointments/create.html'
+    success_url = reverse_lazy('appointment-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments_navbar'] = "selected"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class AppointmentDetailView(DetailView):
+    model = Appointment
+    template_name = 'appointments/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments_navbar'] = "selected"
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class AppointmentUpdateView(UpdateView):
+    model = Appointment
+    fields = "__all__"
+    template_name = 'appointments/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments_navbar'] = "selected"
+        return context
+
+    def get_success_url(self):
+        return reverse('appointment-detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(login_required, name='dispatch')
+class AppointmentDeleteView(DeleteView):
+    model = Appointment
+    template_name = 'appointments/delete.html'
+    success_url = reverse_lazy('appointment-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['appointments_navbar'] = "selected"
+        return context
