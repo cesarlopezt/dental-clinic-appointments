ec79d88d523de95132540148899d65ae8531b669
calt08 <cesarlopez2808@gmail.com>
2021-04-11 23:49:02 -0400

    Add minvalue to phone and ids

--- appointments/models.py
+++ appointments/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.core.validators import MinLengthValidator
+
 
 class Clinic(models.Model):
     name = models.CharField(max_length=30)
@@ -14,8 +16,10 @@ class Dentist(models.Model):
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    id_card = models.CharField(max_length=11)
-    phone_no = models.CharField(max_length=10)
+    id_card = models.CharField(max_length=11, validators=[
+                               MinLengthValidator(11)])
+    phone_no = models.CharField(max_length=10, validators=[
+                                MinLengthValidator(10)])
     email = models.CharField(max_length=75)
     birth_date = models.DateField()
     registration_date = models.DateTimeField(auto_now_add=True, blank=True)
@@ -29,8 +33,10 @@ class Secretary(models.Model):
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    id_card = models.CharField(max_length=11)
-    phone_no = models.CharField(max_length=10)
+    id_card = models.CharField(max_length=11, validators=[
+                               MinLengthValidator(11)])
+    phone_no = models.CharField(max_length=10, validators=[
+                                MinLengthValidator(10)])
     email = models.CharField(max_length=75)
     birth_date = models.DateField()
     registration_date = models.DateTimeField(auto_now_add=True, blank=True)
@@ -53,8 +59,10 @@ class Patient(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    id_card = models.CharField(max_length=11, null=True)
-    phone_no = models.CharField(max_length=10)
+    id_card = models.CharField(max_length=11, null=True, validators=[
+                               MinLengthValidator(11)])
+    phone_no = models.CharField(max_length=10, validators=[
+                                MinLengthValidator(10)])
     email = models.CharField(max_length=75)
     birth_date = models.DateField()
     registration_date = models.DateTimeField(auto_now_add=True, blank=True)
